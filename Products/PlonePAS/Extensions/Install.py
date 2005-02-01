e3c0f6c14adfb3be05b25f0355f85dd7954d4ba6
Kapil Thangavelu <k_vertigo@users.sourceforge.net>
2005-02-01 21:12:38 +0000

    add cmfish roles
    svn path=/trunk/; revision=9550

--- Products/PlonePAS/Extensions/Install.py
+++ Products/PlonePAS/Extensions/Install.py
@@ -24,20 +24,35 @@ def activatePluginInterfaces(portal, plugin, out):
 
 def configurePlonePAS(portal, out):
     """Add the necessary objects to make a usable PAS instance"""
+    setupPlugins( portal )
+    setupRoles( portal )
+
+def setupRoles( portal ):
+    rmanager = portal.acl_users.role_manager
+    rmanager.addRole('Member', title="portal member")
+    rmanager.addRole('Reviewer', title="content reviewer")
+
+def setupPlugins( portal )
     pas = portal.acl_users
     pas.manage_addProduct['PluggableAuthService'].addCookieAuthHelper('cookie_auth', cookie_name='__ac')
+    pas.cookie_auth.manage_activateInterfaces( pas.cookie_auth.listInterfaces() )
+    activatePluginInterfaces(portal, 'cookie_auth', out)    
     print >> out, "Added Cookie Auth Helper."
-    activatePluginInterfaces(portal, 'cookie_auth', out)
+
     
     pas.manage_addProduct['PluggableAuthService'].addZODBUserManager('zodb_users')
+    pas.zodb_users.manage_activateInterfaces( pas.zodb_users.listInterfaces() )
+    activatePluginInterfaces(portal, 'zodb_users', out)    
     print >> out, "Added ZODB User Manager."
-    activatePluginInterfaces(portal, 'zodb_users', out)
+
     
-    pas.manage_addProduct['PluggableAuthService'].addZODBRoleManager('role_mgr')
-    print >> out, "Added ZODB Role Manager."
+    pas.manage_addProduct['PluggableAuthService'].addZODBRoleManager('role_manager')
+    pas.role_mgr.manage_activateInterfaces( pas.role_mgr.listInterfaces() )
     activatePluginInterfaces(portal, 'role_mgr', out)
-    
+    print >> out, "Added ZODB Role Manager."
+
     pas.manage_addProduct['PluggableAuthService'].addZODBGroupManager('zodb_groups')
+    pas.zodb_groups.manage_activateInterfaces( pas.zodb_groups.listInterfaces() )
     print >> out, "Added ZODB Group Manager."
     activatePluginInterfaces(portal, 'zodb_groups', out)
 
