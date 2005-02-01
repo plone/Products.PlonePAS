c8ec22fcb415a5c0389f03b2fd6a59125a67e8d9
webmaven <webmaven@localhost>
2005-02-01 20:04:52 +0000

    *** empty log message ***
    svn path=/trunk/; revision=9549

--- Products/PlonePAS/Extensions/Install.py
+++ Products/PlonePAS/Extensions/Install.py
@@ -5,17 +5,41 @@ from Products.Archetypes.Extensions.utils import install_subskin
 
 from Acquisition import aq_base
 
+def activatePluginInterfaces(portal, plugin, out):
+    pas = portal.acl_users
+    plugin_obj = pas[plugin]
+
+    interfaces = plugin_obj.listInterfaces()
+
+    activatable = []
+
+    for info in plugin_obj.plugins.listPluginTypeInfo():
+        interface = info['interface']
+        interface_name = info['id']
+        if plugin_obj.testImplements(interface):
+            activatable.append(interface_name)
+            print >> out, "  Activating: " + info['title']
+    plugin_obj.manage_activateInterfaces(activatable)
+    print >> out, plugin + " activated."
+
 def configurePlonePAS(portal, out):
     """Add the necessary objects to make a usable PAS instance"""
     pas = portal.acl_users
     pas.manage_addProduct['PluggableAuthService'].addCookieAuthHelper('cookie_auth', cookie_name='__ac')
     print >> out, "Added Cookie Auth Helper."
+    activatePluginInterfaces(portal, 'cookie_auth', out)
+    
     pas.manage_addProduct['PluggableAuthService'].addZODBUserManager('zodb_users')
     print >> out, "Added ZODB User Manager."
+    activatePluginInterfaces(portal, 'zodb_users', out)
+    
     pas.manage_addProduct['PluggableAuthService'].addZODBRoleManager('role_mgr')
     print >> out, "Added ZODB Role Manager."
+    activatePluginInterfaces(portal, 'role_mgr', out)
+    
     pas.manage_addProduct['PluggableAuthService'].addZODBGroupManager('zodb_groups')
     print >> out, "Added ZODB Group Manager."
+    activatePluginInterfaces(portal, 'zodb_groups', out)
 
 
 def install(self):
