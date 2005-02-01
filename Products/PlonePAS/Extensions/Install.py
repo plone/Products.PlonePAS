e3055048188d06868f0d585d314940fde48f31c5
webmaven <webmaven@localhost>
2005-02-01 02:12:10 +0000

    *** empty log message ***
    svn path=/trunk/; revision=9542

--- Products/PlonePAS/Extensions/Install.py
+++ Products/PlonePAS/Extensions/Install.py
@@ -3,9 +3,23 @@ from Products.CMFCore.utils import getToolByName
 from Products.PlonePAS import config
 from Products.Archetypes.Extensions.utils import install_subskin
 
+from Acquisition import aq_base
 
 def configurePlonePAS(portal, out):
-    pass
+    """Add the necessary objects to make a usable PAS instance"""
+    pas = portal.acl_users
+    pas.manage_addProduct['PluggableAuthService'].addCookieAuthHelper('cookie_auth', cookie_name='__ac')
+    print >> out, "Added Cookie Auth Helper."
+    pas.manage_addProduct['PluggableAuthService'].addZODBUserManager('zodb_users')
+    print >> out, "Added ZODB User Manager."
+    pas.manage_addProduct['PluggableAuthService'].addZODBRoleManager('role_mgr')
+    print >> out, "Added ZODB Role Manager."
+    pas.manage_addProduct['PluggableAuthService'].addZODBGroupManager('zodb_groups')
+    print >> out, "Added ZODB Group Manager."
+    pas.manage_addProduct['PluggableAuthService'].addLocalRolePlugin('local_roles')
+    print >> out, "Added Local Role Plugin."
+    pas.manage_addProduct['PluggableAuthService'].addScriptablePlugin('scriptables')
+    print >> out, "Added Scriptable Plugin."
 
 
 def install(self):
@@ -13,8 +27,11 @@ def install(self):
     portal = getToolByName(self, 'portal_url').getPortalObject()
 
     install_subskin(self, out, config.GLOBALS)
-    print >> out, "Install skins."
+    print >> out, "Installed skins."
 
+    if not hasattr(aq_base(portal), 'acl_users'):
+        portal.manage_addProduct['PluggableAuthService'].addPluggableAuthService()
+    
     configurePlonePAS(portal, out)
 
 
