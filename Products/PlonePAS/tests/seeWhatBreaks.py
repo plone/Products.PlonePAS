7fe3953bd1cc70ea9c6b4d1e93c06471fe93bd99
Chad Whitacre <chad@zetaweb.com>
2005-02-02 20:51:57 +0000

    clean up and doc it
    svn path=/trunk/; revision=9563

--- Products/PlonePAS/tests/seeWhatBreaks.py
+++ Products/PlonePAS/tests/seeWhatBreaks.py
@@ -1,38 +1,48 @@
 """
 
-run all CMFPlone tests with our PAS user folder instead of the standard GRUF
+This script runs all CMFPlone tests with our PAS user folder instead of the
+standard GRUF in order to flush out changes that need to be made. Usage:
+
+% python seeWhatBreaks.py testFoo testBar
+
+The arguments are module names in CMFPlone/tests. They are imported (as opposed
+to execfile'd) so you don't need the '.py' extension. If no args are given, all
+tests will be run.
 
 """
 
+# framework is copied over from CMFPlone/tests
 import os, sys
 if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
 
-# put our PloneTestCase in CMFPlone/runallatest.py's way
+# use our PloneTestCase, not CMFPlone's
 import PloneTestCase
 from Products.CMFPlone.tests import PloneTestCase as BaseModule
 BaseModule.PloneTestCase = PloneTestCase.PloneTestCase
 
-# run CMFPlone's tests, not ours
+# but run CMFPlone's tests, not ours
 from Products.CMFCore import utils
 CMFPLONE_TESTS = os.sep.join(('CMFPlone','tests'))
 CMFPLONE_TESTS = utils.expandpath(CMFPLONE_TESTS)
 
-# and now actually run the tests
+# prime our test runner
 import unittest
 TestRunner = unittest.TextTestRunner
 suite = unittest.TestSuite()
 
+# get our tests
 tests = sys.argv[1:]
 if not tests:
     tests = os.listdir(CMFPLONE_TESTS)
-    tests = [n[:-3] for n in tests if n.startswith('test') and n.endswith('.py')]
-
+    tests = [n[:-3] for n in tests if n.startswith('test') \
+                                  and n.endswith('.py')]
 for test in tests:
     g = globals()
     m = __import__('Products.CMFPlone.tests.'+test,g,g,test)
     if hasattr(m, 'test_suite'):
         suite.addTest(m.test_suite())
 
+# ... and run them!
 if __name__ == '__main__':
     TestRunner().run(suite)
