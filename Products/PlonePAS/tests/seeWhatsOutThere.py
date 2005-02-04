b51d93b0230aae7027e8906f63495bc9d87b3b16
Chad Whitacre <chad@zetaweb.com>
2005-02-04 19:31:02 +0000

    caching and tracking
    svn path=/trunk/; revision=9583

--- Products/PlonePAS/tests/seeWhatsOutThere.py
+++ Products/PlonePAS/tests/seeWhatsOutThere.py
@@ -10,58 +10,71 @@ list of GRUFisms will be returned. If no arguments are given, the script will
 walk all of CMFPlone and will return a summary count of GRUFisms per file
 (.pyc's and .svn dirs are always skipped).
 
-GRUFisms are API in GRUF that aren't in our implementation of PAS.
+GRUFisms are API in GRUF that aren't in our implementation of PAS. The set of
+difference between GRUF and PAS are cached; if PAS changes and you need to
+refresh, just delete whatsOutThere.dat or pass in 'refresh' as the first arg.
 
-$Id: seeWhatsOutThere.py,v 1.2 2005/02/04 10:43:51 whit537 Exp $
+$Id: seeWhatsOutThere.py,v 1.3 2005/02/04 19:31:02 whit537 Exp $
 """
+import os, sys
 from pprint import pprint
 from sets import Set
-from os.path import join
+from os.path import join, isfile
+from pickle import dump, load
 
+datafile = 'whatsOutThere.dat'
+refresh = sys.argv[1:2] == ['refresh']
 
 # framework is copied over from CMFPlone/tests
-import os, sys
 if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
 
+if isfile(datafile) and not refresh:
 
-# Set up a Plone site. I couldn't get this to work with PloneTestCase but I
-#  imagine that would be the first optimization. Another option would be to
-#  cache the diff in a file, and only refresh the diff if explicitly asked to.
-print 'installing Zope ...'
-import Zope
-from AccessControl.SecurityManagement import newSecurityManager
-app = Zope.app()
-app.acl_users._doAddUser('fooUser', '', ['Manager'], [])
-user = app.acl_users.getUserById('fooUser').__of__(app.acl_users)
-newSecurityManager(None, user)
-app.manage_addProduct['CMFPlone'].manage_addSite('portal')
+    print "loading cached inspection results ..."
+    diff = load(file(datafile))
 
+else:
 
-# get all the methods from GRUF
-print 'inspecting GRUF ...'
-gruf = Set(dir(app.portal.acl_users))
+    if refresh:
+        del sys.argv[1]
 
+    # Set up a Plone site. I couldn't get this to work with PloneTestCase but I
+    #  imagine that would be the first optimization. Another option would be to
+    #  cache the diff in a file, and only refresh the diff if explicitly asked to.
+    print 'installing Zope ...'
+    import Zope
+    from AccessControl.SecurityManagement import newSecurityManager
+    app = Zope.app()
+    app.acl_users._doAddUser('fooUser', '', ['Manager'], [])
+    user = app.acl_users.getUserById('fooUser').__of__(app.acl_users)
+    newSecurityManager(None, user)
+    app.manage_addProduct['CMFPlone'].manage_addSite('portal')
 
-# get the API available in the new PAS-based acl_users
-print 'inspecting PAS ...'
-app.portal.manage_delObjects(['acl_users'])
-app.portal.portal_quickinstaller.installProduct('PlonePAS')
-pas = Set(dir(app.portal.acl_users))
+    # get all the methods from GRUF
+    print 'inspecting GRUF ...'
+    gruf = Set(dir(app.portal.acl_users))
 
+    # get the API available in the new PAS-based acl_users
+    print 'inspecting PAS ...'
+    app.portal.manage_delObjects(['acl_users'])
+    app.portal.portal_quickinstaller.installProduct('PlonePAS')
+    pas = Set(dir(app.portal.acl_users))
 
-# get the elements which are in GRUF but not PAS
-diff = gruf - pas
-del(app)
+    # get the elements which are in GRUF but not PAS
+    diff = gruf - pas
+    del(app)
 
+    # serialize the info so we don't have to wait for Zope each time
+    dump(diff, file(datafile,'w'))
 
-# tell someone about it
-print """\
-results of our inspections:
-  GRUF: %s
-  PAS:  %s
-  diff: %s"""\
-% (len(gruf), len(pas), len(diff))
+    # tell someone about it
+    print """\
+    results of our inspections:
+      GRUF: %s
+      PAS:  %s
+      diff: %s"""\
+    % (len(gruf), len(pas), len(diff))
 
 
 # get ready to search through some files
@@ -95,7 +108,7 @@ if tests:
                 print attr
         print
 
-else:
+elif refresh:
     # if we don't have args then walk the entire CMFPlone tree; output a summary
     totalGRUFiles = totalattrs = 0
 
