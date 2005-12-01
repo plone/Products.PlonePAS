a4751bda38beb203185377afff9ebb07f8bdb660
J. Cameron Cooper <jccooper@gmail.com>
2005-12-01 19:10:23 +0000

    the title of the folder should not show the quoted name, even though the id must be quoted
    svn path=/PlonePAS/trunk/; revision=14469

--- Products/PlonePAS/tools/membership.py
+++ Products/PlonePAS/tools/membership.py
@@ -306,7 +306,7 @@ class MembershipTool(BaseMembershipTool):
 
         member_folder_title = translate(
             'plone', 'title_member_folder',
-            {'member': safe_member_id}, self,
+            {'member': member_id}, self,
             default = "%s's Home" % safe_member_id)
 
         member_folder_description = translate(
