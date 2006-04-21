0c2cde6cc824e23a40810f81ebd3e29ca72c5c88
Wichert Akkerman <wichert@wiggy.net>
2006-04-21 08:16:50 +0000

    Revert that change - even though it makes the code hard to follow it is a proper optimization
    svn path=/PlonePAS/branches/plonepas-1.x/; revision=22516

--- Products/PlonePAS/plugins/ufactory.py
+++ Products/PlonePAS/plugins/ufactory.py
@@ -186,7 +186,7 @@ class PloneUser(PropertiedUser):
             if role in user_roles:
                 if self._check_context(object):
                     return 1
-                break
+                return None
 
         # check for local roles
         lrmanagers = self._getLocalRolesPlugins()
