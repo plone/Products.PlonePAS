c60c1f7f2a8f94d8a235895c4d3fbd381cbe516e
rochael <rochael@users.sourceforge.net>
2006-07-11 22:56:18 +0000

    merging https://svn.plone.org/svn/collective/PlonePAS/branches/rochael-2.x-ldapmp-fixes : Make use of newly implemented LDAPMP.searchGroups() instead of trying to search the groups directly WARNING: this change require LDAPMultiPlugins version 1.3 or higher. This version hasn't been released yet at the time of this commit
    svn path=/PlonePAS/trunk/; revision=26036

--- Products/PlonePAS/ldapmp.py
+++ Products/PlonePAS/ldapmp.py
@@ -21,22 +21,24 @@ from Products.PlonePAS.plugins.group import PloneGroup
 from Products.LDAPUserFolder.utils import GROUP_MEMBER_MAP
 from Products.LDAPUserFolder.LDAPDelegate import filter_format
 from Products.LDAPMultiPlugins.LDAPPluginBase import LDAPPluginBase
+from Products.LDAPMultiPlugins.LDAPMultiPlugin import LDAPMultiPlugin
+
+from sets import Set
 
 GROUP_PROPERTY_MAP = {
-    # target property: ((possible key, as-is),)
-    'title': (('name', False),
-              ('displayName', False),
-              ('cn', False)),
-    'description': (('description', False),),
-    'email': (('mail', False),),
+    # target property: (possible key, ...)
+    'title': ('name',
+              'displayName',
+              'cn',),
+    'description': ('description', ),
+    'email': ('mail', ),
     }
 
 KNOWN_ATTRS = []
 for attrs in GROUP_PROPERTY_MAP.values():
-    for attr, as_is in attrs:
-        if attr in KNOWN_ATTRS:
-            continue
+    for attr in attrs:
         KNOWN_ATTRS.append(attr)
+KNOWN_ATTRS = Set(KNOWN_ATTRS)
 
 def getPropertiesForUser(self, user, request=None):
     """Fullfill PropertiesPlugin requirements
@@ -47,50 +49,14 @@ def getPropertiesForUser(self, user, request=None):
         return call(self, 'getPropertiesForUser', user=user, request=request)
 
     # We've got a PloneGroup.
-    acl = self._getLDAPUserFolder()
-
-    if acl is None or acl._local_groups:
-        # acl._local_groups == 1 means groups not stored on LDAP
-        return ()
-
-    group_filter = [filter_format('(%s=%s)', ('objectClass', o))
-                    for o in GROUP_MEMBER_MAP.keys()]
-    search_str = '(|%s)' % ''.join(group_filter)
-
-    name = getattr(self, 'groupid_attr', 'cn')
-    value = user.getId()
-    search_str = '(&%s%s)' % (search_str, filter_format('(%s=%s)', (name, value)))
-
-    delegate = acl._delegate
-
-    if acl._binduid_usage > 0:
-        bind_dn = acl._binduid
-        bind_pwd = acl._bindpwd
-    else:
-        bind_dn = bind_pwd = ''
-
-    R = delegate.search(acl.getProperty('groups_base'),
-                        acl.getProperty('groups_scope'),
-                        filter=search_str,
-                        attrs=KNOWN_ATTRS,
-                        bind_dn=bind_dn,
-                        bind_pwd=bind_pwd)
-
-    if R['exception']:
-        raise RuntimeError, R['exception']
-
-    groups = R['results']
-
-    properties = {}
+    # Search for groups instead of users
+    groups = self.enumerateGroups(id=user.getId(), exact_match=True)
     # XXX Should we assert there's only one group?
+    properties = {}
     for group in groups:
         for pname, attrs in GROUP_PROPERTY_MAP.items():
-            for attr, as_is in attrs:
-                if not group.has_key(attr):
-                    continue
-                value = group[attr]
-                if not as_is:
-                    value = value[0]
+            for attr in attrs:
+                value = group.get(attr)
                 if value:
                     # Break on first found
                     properties[pname] = value
@@ -99,3 +65,14 @@ def getPropertiesForUser(self, user, request=None):
     return properties
 
 wrap_method(LDAPPluginBase, 'getPropertiesForUser', getPropertiesForUser)
+
+def getGroupsForPrincipal(self, user, request=None, attr=None):
+    """ Fulfill GroupsPlugin requirements, but don't return any groups for groups """
+
+    if not isinstance(user, PloneGroup):
+        # It's not a PloneGroup, continue as usual
+        return call(self, 'getGroupsForPrincipal', user, request=request, attr=attr)
+
+    return ()
+
+wrap_method(LDAPMultiPlugin, 'getGroupsForPrincipal', getGroupsForPrincipal)
