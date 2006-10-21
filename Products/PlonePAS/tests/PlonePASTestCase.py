6707aeadf2eb75e205580d465bf48543753db894
Hanno Schlichting <hanno@hannosch.eu>
2006-10-21 11:12:18 +0000

    Shut down some more deprecation warnings.
    svn path=/PlonePAS/trunk/; revision=32112

--- Products/PlonePAS/tests/PlonePASTestCase.py
+++ Products/PlonePAS/tests/PlonePASTestCase.py
@@ -4,16 +4,8 @@
 
 # $Id: PlonePASTestCase.py 18188 2006-01-19 20:48:45Z wichert $
 
-
-from zope.app.tests.placelesssetup import setUp, tearDown
-from Products.Five import zcml
-import Products.statusmessages
-
-from Testing import ZopeTestCase
 from Products.PloneTestCase import PloneTestCase
-from Products.PloneTestCase.setup import PLONE25
-
-PloneTestCase.setupPloneSite(products=('PlonePAS',))
+PloneTestCase.setupPloneSite()
 
 from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
 
