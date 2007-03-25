89c31a75f97431e75eb6b62101879ec14844c9f6
Wichert Akkerman <wichert@wiggy.net>
2007-03-25 00:12:21 +0000

    install plone.session as well
    svn path=/PlonePAS/trunk/; revision=39699

--- Products/PlonePAS/tests/PlonePASTestCase.py
+++ Products/PlonePAS/tests/PlonePASTestCase.py
@@ -4,10 +4,13 @@
 
 # $Id: PlonePASTestCase.py 18188 2006-01-19 20:48:45Z wichert $
 
+from Testing import ZopeTestCase
 from Products.PloneTestCase import PloneTestCase
 PloneTestCase.setupPloneSite()
 
 from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
+
+ZopeTestCase.installProduct('plone.session')
 
 # Stub test case just in case we need custom stuff at some point
 class PlonePASTestCase(PloneTestCase.PloneTestCase):
