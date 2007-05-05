8b1764c4a00830fe52a971c807218a1e25b39041
Hanno Schlichting <hanno@hannosch.eu>
2007-05-05 08:30:54 +0000

    First part of new package layout.
    svn path=/PlonePAS/trunk/; revision=41682

---
+++ Products/PlonePAS/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '3.0b6'
+
+setup(name='Products.PlonePAS',
+      version=version,
+      description="PlonePAS adapts the PluggableAuthService for use by Plone.",
+      long_description="""\
+      """,
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+      ],
+      keywords='Zope CMF Plone PAS authentication',
+      author='Kapil Thangavelu',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/collective/PlonePAS/trunk',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      download_url='http://plone.org/products/plonepas',
+      install_requires=[
+        'setuptools',
+      ],
+)
