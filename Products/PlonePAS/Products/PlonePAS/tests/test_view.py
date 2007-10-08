254b4db23ca66b4ee18fee837de0c53d10940f2e
Wichert Akkerman <wichert@wiggy.net>
2007-10-08 22:25:02 +0000

    doctests just make debugging harder, so use a normal test instead
    svn path=/PlonePAS/trunk/; revision=51141

--- Products/PlonePAS/Products/PlonePAS/tests/test_view.py
+++ Products/PlonePAS/Products/PlonePAS/tests/test_view.py
@@ -1,16 +1,18 @@
 import unittest
-import doctest
-
-from Testing.ZopeTestCase import ZopeDocFileSuite
 from PlonePASTestCase import PlonePASTestCase
+from Products.PlonePAS.browser.search import PASSearchView
+
+class TestSearchView(PlonePASTestCase):
+    def testBasicSearch(self):
+        view=PASSearchView(None, None)
+        self.assertEqual(results, [{'userid': 'foo'}, {'userid': 'bar'}])
+
+    def testSortingSearch(self):
+        view=PASSearchView(None, None)
+        self.assertEqual(view.sort(results, 'userid'), [{'userid': 'bar'}, {'userid': 'foo'}])
+
 
 def test_suite():
-    suite = ZopeDocFileSuite(
-        'browser/search.txt',
-        package='Products.PlonePAS',
-        optionflags=doctest.ELLIPSIS|doctest.NORMALIZE_WHITESPACE,
-        test_class=PlonePASTestCase)
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestSearchView))
     return suite
-
-if __name__ == "__main__":
-    unittest.main(defaultTest='test_suite')
