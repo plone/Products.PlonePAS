a498f9fa076844b7d2fdc7537d7ce81cb15796e7
Erik Rose <psucorp@grinchcentral.com>
2007-10-08 18:30:13 +0000

    Merged [50855] from trunk.
    svn path=/PlonePAS/branches/3.x/; revision=51124

--- Products/PlonePAS/Products/PlonePAS/interfaces/browser.py
+++ Products/PlonePAS/Products/PlonePAS/interfaces/browser.py
@@ -17,13 +17,26 @@ class IPASInfoView(Interface):
 
 class IPASSearchView(Interface):
 
-    def searchUsers(sort_by=None, **criteria):
+    def searchUsers(sort_by=None, any_field=None, **criteria):
         """Search for users matching a set of criteria.
 
-        The criteria are a dictionary mapping user properties to values.
+        The criteria are a dictionary mapping user properties to values and
+        have the semantics declared by IPluggableAuthService.searchUsers().
         Duplicate results returned by PAS are filtered so only the first
         result remains in the result set. The results can be sorted on
         sort_bys (case insensitive).
+        
+        In addition, a single search string can be sought in multiple
+        fields at a time by passing any_field='your-string'. The fields
+        that will be searched are not explicitly enumerated but will be the
+        user-facing ones a user would expect to search, such as login name
+        and full name. (Perhaps any_field should/could be moved into PAS
+        proper. This could be done without breaking code that depends on
+        this interface.)
+        
+        If you specify both any_field and other criteria that include a
+        field that any_field would typically search (such as login name),
+        the other criteria will be enforced at the expense of any_field.
         """
 
 
@@ -42,7 +55,9 @@ class IPASSearchView(Interface):
     def searchGroups(**criteria):
         """Search for groups matching a set of criteria.
 
-        The criteria are a dictionary mapping group properties to values. 
+        The criteria are a dictionary mapping group properties
+        to values and have the semantics declared by
+        IPluggableAuthService.searchGroups().
         """
 
 
