29b467a2d3c0849e93fb6530a4f287ed3bb80067
Vincent Fretin <vincent.fretin@gmail.com>
2009-09-06 08:08:50 +0000

    Prepare release
    svn path=/Products.PlonePAS/branches/3.x/; revision=96800

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
-version = '3.9'
+version = '3.10'
 
 setup(name='Products.PlonePAS',
       version=version,
       description="PlonePAS adapts the PluggableAuthService for use by Plone.",
-      long_description="""\
-      """,
+      long_description=open("Products/PlonePAS/HISTORY.txt").read(),
       classifiers=[
         "Framework :: Plone",
         "Framework :: Zope2",
@@ -15,13 +14,12 @@ setup(name='Products.PlonePAS',
       keywords='Zope CMF Plone PAS authentication',
       author='Kapil Thangavelu, Wichert Akkerman',
       author_email='plone-developers@lists.sourceforge.net',
-      url='http://svn.plone.org/svn/collective/Products.PlonePAS/trunk',
+      url='http://svn.plone.org/svn/collective/Products.PlonePAS/branches/3.x',
       license='ZPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
       zip_safe=False,
-      download_url='http://plone.org/products/plonepas',
       install_requires=[
         'setuptools',
         'Products.GenericSetup',
