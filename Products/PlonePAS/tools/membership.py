3a633b43fa4a62ee333d2b2693bad359727b38c9
Eric Steele <EricSteele47@gmail.com>
2013-01-25 15:48:18 +0200

    Pull all methods in from base classes.

--- Products/PlonePAS/tools/membership.py
+++ Products/PlonePAS/tools/membership.py
@@ -1,3 +1,5 @@
+from OFS.Folder import Folder
+from warnings import warn
 import logging
 from cStringIO import StringIO
 
@@ -8,22 +10,27 @@ from zope.interface import implements
 
 from DateTime import DateTime
 from App.class_init import InitializeClass
+from App.Dialogs import MessageDialog
 from App.special_dtml import DTMLFile
 from OFS.Image import Image
+from Persistence import PersistentMapping
 
 from AccessControl import ClassSecurityInfo
 from AccessControl import getSecurityManager
-
 from AccessControl import Unauthorized
 from AccessControl.SecurityManagement import noSecurityManager
 from AccessControl.requestmethod import postonly
 from AccessControl.User import nobody
+from Acquisition import aq_base
 from Acquisition import aq_get
 from Acquisition import aq_inner
 from Acquisition import aq_parent
 from zExceptions import BadRequest
 from ZODB.POSException import ConflictError
 
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.CMFCore.exceptions import AccessControl_Unauthorized
+from Products.CMFCore.permissions import ChangeLocalRoles
 from Products.CMFCore.permissions import ManagePortal
 from Products.CMFCore.permissions import ManageUsers
 from Products.CMFCore.permissions import SetOwnProperties
@@ -32,13 +39,11 @@ from Products.CMFCore.permissions import View
 from Products.CMFCore.permissions import ListPortalMembers
 from Products.CMFCore.utils import _checkPermission
 from Products.CMFCore.utils import getToolByName
-from Products.CMFCore.MembershipTool import MembershipTool as BaseTool
 
 from Products.PlonePAS.events import UserLoggedInEvent
 from Products.PlonePAS.events import UserInitialLoginInEvent
 from Products.PlonePAS.events import UserLoggedOutEvent
 from Products.PlonePAS.interfaces import membership
-from Products.PlonePAS.interfaces.group import IGroupTool
 from Products.PlonePAS.utils import cleanId
 from Products.PlonePAS.utils import scale_image
 
@@ -46,7 +51,7 @@ default_portrait = 'defaultUser.png'
 logger = logging.getLogger('PlonePAS')
 
 
-class MembershipTool(BaseTool):
+class MembershipTool(Folder):
     """PAS-based customization of MembershipTool.
     """
 
@@ -65,15 +70,19 @@ class MembershipTool(BaseTool):
     user_search_keywords = ('login', 'fullname', 'email', 'exact_match',
                             'sort_by', 'max_results')
 
-    _properties = (getattr(BaseTool, '_properties', ()) +
-                   ({'id': 'user_search_keywords',
+    _properties = (({'id': 'user_search_keywords',
                      'type': 'lines',
                      'mode': 'rw',
                      },))
 
-    manage_options = (BaseTool.manage_options
-                        + ({'label': 'Portraits',
-                            'action': 'manage_portrait_fix'},))
+    manage_options=( ({ 'label' : 'Configuration'
+                     , 'action' : 'manage_mapRoles'
+                     },) +
+                   ( { 'label' : 'Overview'
+                     , 'action' : 'manage_overview'
+                     },) +
+                   ( { 'label': 'Portraits'
+                     , 'action': 'manage_portrait_fix'},))
 
     # TODO I'm not quite sure why getPortalRoles is declared 'Managed'
     #    in CMFCore.MembershipTool - but in Plone we are not so anal ;-)
@@ -264,8 +273,36 @@ class MembershipTool(BaseTool):
         logger.debug('searchForMembers: finished.')
         return res
 
-    #############
-    ## sanitize home folders (we may get URL-illegal ids)
+    security.declareProtected(ManagePortal, 'getMemberareaCreationFlag')
+    def getMemberareaCreationFlag(self):
+        """
+        Returns the flag indicating whether the membership tool
+        will create a member area if an authenticated user from
+        an underlying user folder logs in first without going
+        through the join process
+        """
+        return self.memberareaCreationFlag
+
+    security.declareProtected(ManagePortal, 'setMemberareaCreationFlag')
+    def setMemberareaCreationFlag(self):
+        """
+        sets the flag indicating whether the membership tool
+        will create a member area if an authenticated user from
+        an underlying user folder logs in first without going
+        through the join process
+        """
+        if not hasattr(self, 'memberareaCreationFlag'):
+            self.memberareaCreationFlag = 0
+
+        if self.memberareaCreationFlag == 0:
+            self.memberareaCreationFlag = 1
+        else:
+            self.memberareaCreationFlag = 0
+
+        return MessageDialog(
+               title='Member area creation flag changed',
+               message='Member area creation flag has been updated',
+               action='manage_mapRoles')
 
     security.declarePublic('createMemberarea')
     def createMemberarea(self, member_id=None, minimal=None):
@@ -275,7 +312,6 @@ class MembershipTool(BaseTool):
         """
         if not self.getMemberareaCreationFlag():
             return None
-        catalog = getToolByName(self, 'portal_catalog')
         membership = getToolByName(self, 'portal_membership')
         members = self.getMembersFolder()
 
@@ -350,6 +386,78 @@ class MembershipTool(BaseTool):
     security.declarePublic('createMemberArea')
     createMemberArea = createMemberarea
 
+    security.declareProtected(ManageUsers, 'deleteMemberArea')
+    @postonly
+    def deleteMemberArea(self, member_id, REQUEST=None):
+        """ Delete member area of member specified by member_id.
+        """
+        members = self.getMembersFolder()
+        if not members:
+            return 0
+        if hasattr(aq_base(members), member_id):
+            members.manage_delObjects(member_id)
+            return 1
+        else:
+            return 0
+
+    security.declarePublic('isAnonymousUser')
+    def isAnonymousUser(self):
+        '''
+        Returns 1 if the user is not logged in.
+        '''
+        u = getSecurityManager().getUser()
+        if u is None or u.getUserName() == 'Anonymous User':
+            return 1
+        return 0
+
+    security.declarePublic('checkPermission')
+    def checkPermission(self, permissionName, object, subobjectName=None):
+        '''
+        Checks whether the current user has the given permission on
+        the given object or subobject.
+        '''
+        if subobjectName is not None:
+            object = getattr(object, subobjectName)
+        return _checkPermission(permissionName, object)
+
+    security.declarePublic('credentialsChanged')
+    def credentialsChanged(self, password, REQUEST=None):
+        '''
+        Notifies the authentication mechanism that this user has changed
+        passwords.  This can be used to update the authentication cookie.
+        Note that this call should *not* cause any change at all to user
+        databases.
+        '''
+        # XXX: this method violates the rules for tools/utilities:
+        # it depends on self.REQUEST
+        if REQUEST is None:
+            REQUEST = self.REQUEST
+            warn("credentialsChanged should be called with 'REQUEST' as "
+                 "second argument. The BBB code will be removed in CMF 2.3.",
+                 DeprecationWarning, stacklevel=2)
+
+        if not self.isAnonymousUser():
+            user = getSecurityManager().getUser()
+            name = user.getUserName()
+            # this really does need to be the user name, and not the user id,
+            # because we're dealing with authentication credentials
+            p = getattr(REQUEST, '_credentials_changed_path', None)
+            if p is not None:
+                # Use an interface provided by CookieCrumbler.
+                change = self.restrictedTraverse(p)
+                change(user, name, password)
+
+    security.declareProtected(ManageUsers, 'getMemberById')
+    def getMemberById(self, id):
+        '''
+        Returns the given member.
+        '''
+        user = self._huntUser(id, self)
+        if user is not None:
+            user = self.wrapUser(user)
+        return user
+
+
     security.declarePublic('getMemberInfo')
     def getMemberInfo(self, memberId=None):
         # Return 'harmless' Memberinfo of any member, such as Full name,
@@ -421,6 +529,130 @@ class MembershipTool(BaseTool):
             return home.absolute_url()
         else:
             return None
+
+
+    def _huntUserFolder(self, member_id, context):
+        """Find userfolder containing user in the hierarchy
+           starting from context
+        """
+        uf = context.acl_users
+        while uf is not None:
+            user = uf.getUserById(member_id)
+            if user is not None:
+                return uf
+            container = aq_parent(aq_inner(uf))
+            parent = aq_parent(aq_inner(container))
+            uf = getattr(parent, 'acl_users', None)
+        return None
+
+    def _huntUser(self, member_id, context):
+        """Find user in the hierarchy of userfolders
+           starting from context
+        """
+        uf = self._huntUserFolder(member_id, context)
+        if uf is not None:
+            return uf.getUserById(member_id)
+
+    def __getPUS(self):
+        """ Retrieve the nearest user folder
+        """
+        warn('__getPUS is deprecated and will be removed in CMF 2.4, '
+             'please acquire "acl_users" instead.', DeprecationWarning,
+             stacklevel=2)
+        return self.acl_users
+
+    security.declareProtected(ListPortalMembers, 'searchMembers')
+    def searchMembers( self, search_param, search_term ):
+        """ Search the membership """
+        # XXX: this method violates the rules for tools/utilities:
+        # it depends on a non-utility tool
+        md = getToolByName( self, 'portal_memberdata' )
+
+        return md.searchMemberData( search_param, search_term )
+
+    security.declareProtected(View, 'setLocalRoles')
+    @postonly
+    def setLocalRoles(self, obj, member_ids, member_role, reindex=1,
+                      REQUEST=None):
+        """ Add local roles on an item.
+        """
+        if ( _checkPermission(ChangeLocalRoles, obj)
+             and member_role in self.getCandidateLocalRoles(obj) ):
+            for member_id in member_ids:
+                roles = list(obj.get_local_roles_for_userid( userid=member_id ))
+
+                if member_role not in roles:
+                    roles.append( member_role )
+                    obj.manage_setLocalRoles( member_id, roles )
+
+        if reindex and hasattr(aq_base(obj), 'reindexObjectSecurity'):
+            obj.reindexObjectSecurity()
+
+    security.declareProtected(View, 'deleteLocalRoles')
+    @postonly
+    def deleteLocalRoles(self, obj, member_ids, reindex=1, recursive=0,
+                         REQUEST=None):
+        """ Delete local roles of specified members.
+        """
+        if _checkPermission(ChangeLocalRoles, obj):
+            for member_id in member_ids:
+                if obj.get_local_roles_for_userid(userid=member_id):
+                    obj.manage_delLocalRoles(userids=member_ids)
+                    break
+
+        if recursive and hasattr( aq_base(obj), 'contentValues' ):
+            for subobj in obj.contentValues():
+                self.deleteLocalRoles(subobj, member_ids, 0, 1)
+
+        if reindex and hasattr(aq_base(obj), 'reindexObjectSecurity'):
+            # reindexObjectSecurity is always recursive
+            obj.reindexObjectSecurity()
+
+    security.declareProtected(ManageUsers, 'deleteMembers')
+    @postonly
+    def deleteMembers(self, member_ids, delete_memberareas=1,
+                      delete_localroles=1, REQUEST=None):
+        """ Delete members specified by member_ids.
+        """
+        # XXX: this method violates the rules for tools/utilities:
+        # it depends on a non-utility tool
+
+        # Delete members in acl_users.
+        acl_users = self.acl_users
+        if _checkPermission(ManageUsers, acl_users):
+            if isinstance(member_ids, basestring):
+                member_ids = (member_ids,)
+            member_ids = list(member_ids)
+            for member_id in member_ids[:]:
+                if not acl_users.getUserById(member_id, None):
+                    member_ids.remove(member_id)
+            try:
+                acl_users.userFolderDelUsers(member_ids)
+            except (AttributeError, NotImplementedError):
+                raise NotImplementedError('The underlying User Folder '
+                                         'doesn\'t support deleting members.')
+        else:
+            raise AccessControl_Unauthorized('You need the \'Manage users\' '
+                                 'permission for the underlying User Folder.')
+
+        # Delete member data in portal_memberdata.
+        mdtool = getToolByName(self, 'portal_memberdata', None)
+        if mdtool is not None:
+            for member_id in member_ids:
+                mdtool.deleteMemberData(member_id)
+
+        # Delete members' home folders including all content items.
+        if delete_memberareas:
+            for member_id in member_ids:
+                 self.deleteMemberArea(member_id)
+
+        # Delete members' local roles.
+        if delete_localroles:
+            self.deleteLocalRoles( getUtility(ISiteRoot), member_ids,
+                                   reindex=1, recursive=1 )
+
+        return tuple(member_ids)
+
 
     security.declarePublic('getPersonalFolder')
     def getPersonalFolder(self, member_id=None):
@@ -508,7 +740,7 @@ class MembershipTool(BaseTool):
         so that it will not be possible anymore to have a method returning
         several hundred of users :)
         '''
-        return BaseTool.listMembers(self)
+        return map(self.wrapUser, self.acl_users.getUsers())
 
     security.declareProtected(ManageUsers, 'listMemberIds')
     def listMemberIds(self):
@@ -640,6 +872,38 @@ class MembershipTool(BaseTool):
         roles.append('Owner')
 
         return roles
+
+    security.declareProtected(ManagePortal, 'setRoleMapping')
+    @postonly
+    def setRoleMapping(self, portal_role, userfolder_role, REQUEST=None):
+        """
+        set the mapping of roles between roles understood by
+        the portal and roles coming from outside user sources
+        """
+        if not hasattr(self, 'role_map'): 
+            self.role_map = PersistentMapping()
+
+        if len(userfolder_role) < 1:
+            del self.role_map[portal_role]
+        else:
+            self.role_map[portal_role] = userfolder_role
+
+        return MessageDialog(
+               title ='Mapping updated',
+               message='The Role mappings have been updated',
+               action ='manage_mapRoles')
+
+    security.declareProtected(ManagePortal, 'getMappedRole')
+    def getMappedRole(self, portal_role):
+        """
+        returns a role name if the portal role is mapped to
+        something else or an empty string if it is not
+        """
+        if hasattr(self, 'role_map'):
+            return self.role_map.get(portal_role, '')
+        else:
+            return ''
+
 
     security.declareProtected(View, 'getCandidateLocalRoles')
     def getCandidateLocalRoles(self, obj):
