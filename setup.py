7cdfea8df4e64bf1dbdb27bee9ae2f90eeb7d081
esteele <eric@esteele.net>
2015-03-21 17:43:00 -0400

    Back to development: 5.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = '5.0.1'
+version = '5.0.2.dev0'
 
 longdescription = open("README.rst").read()
 longdescription += '\n'
