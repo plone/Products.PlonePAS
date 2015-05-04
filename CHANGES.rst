3264712b69fcfcd5b851bc1e8a4d906e77e0fd80
Maurits van Rees <maurits@vanrees.org>
2015-05-04 17:27:27 +0200

    Preparing release 5.0.2
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-5.0.2 (unreleased)
+5.0.2 (2015-05-04)
 ------------------
 
 - Prevent CRSF protecting errors when logging out because of
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = '5.0.2.dev0'
+version = '5.0.2'
 
 longdescription = open("README.rst").read()
 longdescription += '\n'
