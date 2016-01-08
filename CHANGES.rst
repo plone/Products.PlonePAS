d0b74d5fa58698bc077d548ab3df2edaf70b2aa4
Maurits van Rees <maurits@vanrees.org>
2016-01-08 18:27:30 +0100

    Back to development: 5.0.8
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,18 @@
 Changelog
 =========
 
+5.0.8 (unreleased)
+------------------
+
+New:
+
+- *add item here*
+
+Fixes:
+
+- *add item here*
+
+
 5.0.7 (2016-01-08)
 ------------------
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools import find_packages
 import sys
 
-version = '5.0.7'
+version = '5.0.8.dev0'
 
 longdescription = open("README.rst").read()
 longdescription += '\n'
