60118a95fbf1deb42d3f4f2df278722a382cccb3
Jens Vagelpohl <jens@alias.ooo>
2016-01-26 12:57:48 +0100

    - moved the IMembershipTool interface from PlonePAS to CMFPlone

--- src/Products/PlonePAS/interfaces/membership.py
+++ src/Products/PlonePAS/interfaces/membership.py
@@ -1,12 +1,4 @@
-# -*- coding: utf-8 -*-
-from Products.CMFCore import interfaces
-
-
-class IMembershipTool(interfaces.IMembershipTool):
-
-    def getMemberInfo(memberId=None):
-        """Return 'harmless' Memberinfo of any member, such as full name,
-        location, etc
-        """
-
-__all__ = (IMembershipTool, )
+deprecated(
+    "Import from Products.CMFPlone.interfaces instead",
+    IMembershipTool="Products.CMFPlone.interfaces:IMembershipTool",
+)
