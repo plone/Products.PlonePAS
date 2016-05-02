ad7378064042500a9109dfd7e48c6ad5bc71bf35
Maurits van Rees <maurits@vanrees.org>
2016-05-02 12:47:43 +0200

    Preparing release 5.0.10
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools import find_packages
 import sys
 
-version = '5.0.10.dev0'
+version = '5.0.10'
 
 longdescription = open("README.rst").read()
 longdescription += '\n'
