c863af06f2c813d4a638fd13f43fa5b6bf4cb93a
Maurits van Rees <maurits@vanrees.org>
2016-05-02 12:48:04 +0200

    Back to development: 5.0.11
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools import find_packages
 import sys
 
-version = '5.0.10'
+version = '5.0.11.dev0'
 
 longdescription = open("README.rst").read()
 longdescription += '\n'
