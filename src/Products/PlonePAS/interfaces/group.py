a3088dca08e4838442f3ca6e3335560bebd15fa0
Jens Vagelpohl <jens@netz.ooo>
2016-05-18 10:44:59 +0200

    - import fixes

--- src/Products/PlonePAS/interfaces/group.py
+++ src/Products/PlonePAS/interfaces/group.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-from zope.interface import Interface
 from zope.deferredimport import deprecated
-
 
 deprecated(
     "Import from Products.PluggableAuthService.interfaces.plugins instead",
