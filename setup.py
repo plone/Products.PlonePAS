2d970e7ee8f1e3bff030660abe4a61cd9e7c55eb
Maurits van Rees <maurits@vanrees.org>
2020-03-28 01:20:09 +0100

    Added Python 3.8 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     keywords='Zope CMF Plone PAS authentication',
     author='Kapil Thangavelu, Wichert Akkerman',
