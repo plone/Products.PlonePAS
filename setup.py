bbc67102319f2036f279806c737218442b92b065
Maurits van Rees <maurits@vanrees.org>
2022-08-31 02:25:56 +0200

    Added Python 3.10 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="Zope CMF Plone PAS authentication",
     author="Kapil Thangavelu, Wichert Akkerman",
