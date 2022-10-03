0b1c75e936722d596c5662d16095e7a7f375ef45
Maurits van Rees <maurits@vanrees.org>
2022-10-03 10:37:55 +0200

    Update classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         "Framework :: Zope :: 5",
         "License :: OSI Approved :: Zope Public License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
