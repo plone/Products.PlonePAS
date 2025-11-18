562a86e7bfd9ae51b0dbb0e563d031c0a222444e
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-11-18 23:53:28 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "8.0.6.dev0"
+version = "9.0.0.dev0"
 
 long_description = (
     f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
